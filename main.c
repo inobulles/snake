33fcc3641523eba910be027723b2da8f8a696f1c
Obiwac <obiwac@gmail.com>
2021-10-30 18:11:31 +0100

    It's a pumpkin, not an apple

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 // images
 
 #include "img/bg.h"
+#include "img/apple.h"
 
 // macros and typedefs
 
@@ -35,6 +36,18 @@ typedef struct {
 	uint8_t* framebuffer;
 } game_t;
 
+static int curr_rand = 1234567890;
+
+static int randint(int a, int b) {
+	curr_rand = (420 * curr_rand + 69) % 0x10001;
+
+	if (curr_rand < 0) {
+		curr_rand = -curr_rand;
+	}
+
+	return curr_rand % (b - a + 1) + a;
+}
+
 static inline tile_t* ref_tile(game_t* game, unsigned x, unsigned y) {
 	return &game->map[y * game->tiles_x + x];
 }
@@ -44,30 +57,34 @@ static inline tile_t* ref_tile(game_t* game, unsigned x, unsigned y) {
 	game->framebuffer[4 * ((y) * game->width + (x)) + 1] = (g); \
 	game->framebuffer[4 * ((y) * game->width + (x)) + 2] = (b);
 
-static void render_image(game_t* game, unsigned img_width, unsigned img_height, uint8_t* img_data, unsigned x, unsigned y, unsigned width, unsigned height) {
+static void render_image(game_t* game, unsigned img_width, unsigned img_height, const uint8_t* img_data, unsigned x, unsigned y, unsigned width, unsigned height) {
 	const unsigned px_x = width  / img_width;
 	const unsigned px_y = height / img_height;
 
-	for (unsigned line = 0; line < height; line++) {
-		const uint8_t* img_line = img_data + line / px_y * img_width * 4;
+	for (unsigned column = 0; column < width; column++) {
+		const uint8_t* img_column = img_data + column / px_x * img_height * 4;
 
-		for (unsigned column = 0; column < width; column++) {
-			uint8_t r = img_line[column / px_x * 4 + 0];
-			uint8_t g = img_line[column / px_x * 4 + 1];
-			uint8_t b = img_line[column / px_x * 4 + 2];
+		for (unsigned line = 0; line < height; line++) {
+			uint8_t b = img_column[line / px_y * 4 + 0];
+			uint8_t g = img_column[line / px_y * 4 + 1];
+			uint8_t r = img_column[line / px_y * 4 + 2];
 			
-			if (img_line[column / px_x * 4 + 3]) { // is opaque?
+			if (img_column[line / px_y * 4 + 3]) { // is opaque?
 				PLOT(line + x, column + y, r, g, b)
 			}
 		}
 	}
 }
 
-#define RENDER_IMAGE(img, x, y, _width, _height) render_image(game, (img).width, (img).height, (img).pixel_data, (x), (y), (_width), (_height));
+#define RENDER_IMAGE(img) render_image(game, (img).width, (img).height, (img).pixel_data, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 
 static void render_tile(game_t* game, tile_t* tile, unsigned x, unsigned y) {
 	if (tile->type == TILE_EMPTY) {
-		RENDER_IMAGE(img_bg, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+		RENDER_IMAGE(img_bg)
+	}
+
+	else if (tile->type == TILE_APPLE) {
+		RENDER_IMAGE(img_apple)
 	}
 }
 
@@ -78,6 +95,14 @@ static void render_world(game_t* game) {
 			render_tile(game, tile, x, y);
 		}
 	}
+}
+
+static void place_apple(game_t* game) {
+	int x = randint(0, game->tiles_x - 1);
+	int y = randint(0, game->tiles_y - 1);
+
+	tile_t* tile = ref_tile(game, x, y);
+	tile->type = TILE_APPLE;
 }
 
 int main(void) {
@@ -92,8 +117,8 @@ int main(void) {
 
 	vga_mode_t* mode = &modes[0];
 
-	mode->width = 1024;
-	mode->height = 768;
+	mode->width = 800;//1024;
+	mode->height = 576;//768;
 
 	vga_set_mode(mode);
 	game->framebuffer = vga_get_framebuffer();
@@ -103,10 +128,14 @@ int main(void) {
 
 	// setup
 
+	curr_rand = (uint64_t) game; // set the seed to something approximatively unpredictable
+
 	game->tiles_x = mode->width  / TILE_SIZE;
 	game->tiles_y = mode->height / TILE_SIZE;
 
 	game->map = calloc(game->tiles_x * game->tiles_y, sizeof *game->map);
+
+	place_apple(game);
 
 	// main loop
 
